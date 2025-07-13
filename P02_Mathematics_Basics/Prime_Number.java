37f55862dedfdaa1bed94bf47beb2ed743927320
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-07-13 20:43:51 +0530

    Prime Number

---
+++ P02_Mathematics_Basics/Prime_Number.java
@@ -0,0 +1,17 @@
+// Given a number n, determine whether it is a prime number or not.
+// Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+package P02_Mathematics_Basics;
+
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if (n <= 1)
+            return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
--- P02_Mathematics_Basics/Sum_of_Natural_Numbers.java
+++ P02_Mathematics_Basics/Sum_of_Natural_Numbers.java
@@ -1,7 +1,7 @@
 // Given an integer n, your task is to compute the sum of all natural numbers from 1 to n (inclusive).
 // If n is 0, the sum should be 0.
 
-package P02_Mathematics;
+package P02_Mathematics_Basics;
 
 public class Sum_of_Natural_Numbers {
     public static int findSum(int n) {
